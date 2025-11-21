106dd8b5706f271010e6ef70d6e6176b546c42ae
InsaustiAlejandro <34839646+KernelN@users.noreply.github.com>
2025-11-20 23:35:38 -0300

    Subscribe gameplay manager to target reached event

---
+++ Assets/Scripts/FuelBank.cs
@@ -0,0 +1,86 @@
+using System;
+using UnityEngine;
+
+namespace ShipIt
+{
+    public class FuelBank : MonoBehaviour
+    {
+        [SerializeField] int launchFuelCost = 1;
+        [SerializeField, Min(0)] int cachedFuel;
+
+        public bool IsFuelDepleted { get; private set; }
+        public event Action OnFuelDepleted;
+
+        GameManager gameManager;
+
+        void Start()
+        {
+            gameManager = GameManager.inst;
+            if (gameManager != null)
+            {
+                GameData data = gameManager.Data;
+                cachedFuel = Mathf.Max(0, data.fuel);
+            }
+
+            if (cachedFuel <= 0)
+            {
+                FlagDepleted();
+            }
+        }
+
+        public bool TryConsumeForLaunch()
+        {
+            if (launchFuelCost <= 0)
+            {
+                return true;
+            }
+
+            if (cachedFuel < launchFuelCost)
+            {
+                if (cachedFuel <= 0)
+                {
+                    FlagDepleted();
+                }
+                return false;
+            }
+
+            cachedFuel -= launchFuelCost;
+            PersistFuel();
+
+            if (cachedFuel <= 0)
+            {
+                FlagDepleted();
+            }
+
+            return true;
+        }
+
+        void PersistFuel()
+        {
+            if (gameManager == null)
+            {
+                return;
+            }
+
+            GameData data = gameManager.Data;
+            data.fuel = cachedFuel;
+            gameManager.SaveGameData();
+        }
+
+        void FlagDepleted()
+        {
+            if (IsFuelDepleted)
+            {
+                return;
+            }
+
+            IsFuelDepleted = true;
+            OnFuelDepleted?.Invoke();
+        }
+
+        void OnDestroy()
+        {
+            PersistFuel();
+        }
+    }
+}
