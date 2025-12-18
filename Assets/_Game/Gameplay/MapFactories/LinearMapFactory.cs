faf5b7e35bf562946325b8765b55d4d2e0599a07
InsaustiAlejandro <alejandroninsausti@gmail.com>
2025-12-18 12:15:46 -0300

    Add LinearMapFactory

---
+++ Assets/_Game/Gameplay/MapFactories/LinearMapFactory.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+namespace ShipIt.Gameplay.Astral
+{
+    [CreateAssetMenu(fileName = "LinearMapFactory", menuName = "ShipIt/MapFactory/Linear Map Factory")]
+    public class LinearMapFactory : MapFactory
+    {
+        [SerializeField] AstralTargetBuilder targetBuilder;
+        [SerializeField, Range(0f, 180f)] float maxRotationAngle = 45f;
+
+        const float k_MinRotationOffset = 0.1f;
+
+        public override int SpawnMap(Transform anchor, int seed)
+        {
+            if (anchor == null || planetFactory == null || planetQuantity <= 0)
+            {
+                LastSeed = 0;
+                return LastSeed;
+            }
+
+            var previousState = Random.state;
+            Random.InitState(seed);
+
+            float minDistance = minDistanceBetweenPlanets;
+            float maxDistance = maxDistanceBetweenPlanets;
+
+            AstralBody firstPlanet = planetFactory.SpawnBody(anchor.position, anchor.rotation);
+            if (firstPlanet == null)
+            {
+                LastSeed = 0;
+                Random.state = previousState;
+                return LastSeed;
+            }
+
+            firstPlanet.AddAstralComponent(componentBuilders[0].GetComponent());
+            firstPlanet.gameObject.name = "Planet 1";
+            Transform prevPlanet = firstPlanet.transform;
+            AstralBody lastPlanet = firstPlanet;
+
+            for (int i = 1; i < planetQuantity; i++)
+            {
+                float distance = Random.Range(minDistance, maxDistance);
+                Vector3 spawnPos = prevPlanet.position + prevPlanet.forward * distance;
+                float rotationLimit = Mathf.Max(k_MinRotationOffset, maxRotationAngle);
+                Quaternion targetRotation = Random.rotation;
+                float rotationStep = Random.Range(k_MinRotationOffset, rotationLimit);
+                Quaternion spawnRot = Quaternion.RotateTowards(prevPlanet.rotation, targetRotation, rotationStep);
+
+                AstralBody planet = planetFactory.SpawnBody(spawnPos, spawnRot);
+                if (planet == null)
+                {
+                    continue;
+                }
+
+                planet.AddAstralComponent(componentBuilders[0].GetComponent());
+                planet.gameObject.name = $"Planet {i + 1}";
+                prevPlanet = planet.transform;
+                lastPlanet = planet;
+            }
+
+            if (lastPlanet && targetBuilder)
+            {
+                targetBuilder.Build(lastPlanet.transform);
+                lastPlanet.AddAstralComponent(targetBuilder.GetComponent());
+            }
+
+            LastSeed = seed;
+            Random.state = previousState;
+
+            return LastSeed;
+        }
+    }
+}
