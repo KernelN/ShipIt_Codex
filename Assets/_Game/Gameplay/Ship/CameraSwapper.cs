feae2a2d3c245a9f5bf1ef139ac4047bdb89884c
InsaustiAlejandro <34839646+KernelN@users.noreply.github.com>
2026-02-12 14:59:37 -0300

    Add CameraSwapper script for camera toggling and animation trigger

---
+++ Assets/_Game/Gameplay/Ship/CameraSwapper.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ShipIt.Gameplay
+{
+    public class CameraSwapper : MonoBehaviour
+    {
+        [Header("Camera setup")]
+        [SerializeField] Camera firstCamera;
+        [SerializeField] Camera secondCamera;
+
+        [Header("Optional UI binding")]
+        [SerializeField] Button swapButton;
+
+        [Header("Animation setup")]
+        [SerializeField] Animator targetAnimator;
+        [SerializeField] string triggerName = "Play";
+
+        bool showFirstCamera = true;
+
+        void Awake()
+        {
+            ApplyCameraState();
+        }
+
+        void OnEnable()
+        {
+            if (swapButton)
+                swapButton.onClick.AddListener(SwapCameraAndPlayAnimation);
+        }
+
+        void OnDisable()
+        {
+            if (swapButton)
+                swapButton.onClick.RemoveListener(SwapCameraAndPlayAnimation);
+        }
+
+        public void SwapCameraAndPlayAnimation()
+        {
+            showFirstCamera = !showFirstCamera;
+            ApplyCameraState();
+
+            if (!targetAnimator || string.IsNullOrEmpty(triggerName))
+                return;
+
+            targetAnimator.ResetTrigger(triggerName);
+            targetAnimator.SetTrigger(triggerName);
+        }
+
+        void ApplyCameraState()
+        {
+            if (firstCamera)
+                firstCamera.gameObject.SetActive(showFirstCamera);
+
+            if (secondCamera)
+                secondCamera.gameObject.SetActive(!showFirstCamera);
+        }
+    }
+}
